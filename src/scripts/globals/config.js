41c5ecc861201d770ce33db44e772e644538520c
Gilang Ap <108512328+gngap-016@users.noreply.github.com>
2022-10-14 18:35:55 +0700

    Update Data

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: '2bdf01fd66ad35396751f8c9fd6d50f5',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
